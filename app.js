4e520d616bdb761e0b380f3af7fa9b6b4d28a162
Aditya Tiwari <adityatiwari0808@gmail.com>
2025-05-27 19:40:54 +0530

    Initial commit

--- app.js
+++ app.js
@@ -3,33 +3,46 @@ const connectDB = require("./src/config/database");
 const app = express();
 const User = require('./src/models/user');
 
+app.use(express.json());
+
+
+
 app.post("/signup",async (req,res)=>{
 
-    const  user = new User({
-        firstName:"Aditya",
-        lastName:"Tiwari",
-        emailId:"adityatiwari0808@gmail.com",
-        password:"adi@0802"
+    console.log(req.body);
+
+    const  user = new User(req.body);
+
+    try {
+        await user.save();
+        res.send("user added succesfully");
+        
+    } catch (err) {
+        res.status(400).send("error saving the user:"+err.message);
+        
+    }
+
+});
 
 
-    });
 
-    await user.save();
-    res.send("user addded succesfylly");
+app.get('/user', async(req,res)=>{
+    const firstname = req.body.firstName;
 
+    try{
+        const user = await User.find({firstName:firstname});
+
+        res.send(user);
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
 })
-
-
-
-
-
-
 
 connectDB()
 .then(()=>{
     console.log("database cnnection esatblished");
-    app.listen(7777,()=>{
-    console.log("server is successfully litening on port 7777");
+    app.listen(3000,()=>{
+    console.log("server is successfully litening on port 3000");
 })
 
 })
